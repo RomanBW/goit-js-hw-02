d70964d46be07d96d7274c90b8da37a99d9bf6b1
RomanBW <romanbw26@gmail.com>
2024-03-01 17:46:16 +0200

    goit-js-hw-02-1

--- js/task-3.js
+++ js/task-3.js
@@ -6,6 +6,7 @@ function checkForSpam(message) {
     return false;
   }
 }
+
 console.log(checkForSpam('Latest technology news'));
 console.log(checkForSpam('JavaScript weekly newsletter'));
 console.log(checkForSpam('Get best sale offers now!'));
